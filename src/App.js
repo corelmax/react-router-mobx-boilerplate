117c92962b612b37981cd2529833eb8ec64c4786
Anucha Petchagun <corelmax@gmail.com>
2019-03-21 18:47:53 +0700

    demo1 code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,114 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AuthStore, { increaseCount } from './AuthStore'
+import { observer } from 'mobx-react';
+import {Route, Switch} from 'react-router'
+import ProfileStore, {getProfileById} from './ProfileStore';
 
+const P = props => (
+  <p>{props.count}</p>
+)
+
+const users = [
+  {
+    id: 1,
+    name: 'test'
+  },
+  {
+    id: 2,
+    name: 'test2'
+  }
+]
+
+export const ProfileSingle2 = observer(props => {
+  if(!props.store.profile) 
+      return <div>Placeholder</div>
+
+    return (
+      <div>this is profile detail of id {props.store.profile.name}</div>
+    )
+})
+
+export const withProfile = Comp => props => {
+  getProfileById(props.match.params.id)
+  return <Comp store={ProfileStore} />
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        this is page Home 
+      </div>
+    )
+  }
+}
+
+const ProfileList = props => (
+  <p>This is list of profile</p>
+)
+
+const ProfileEdit = props => (
+  <p>This is edit profile page</p>
+)
+
+class Profile extends Component {
+  render() {
+    return (
+      <div >
+        <div style={{
+          backgroundColor: '#f3e'
+        }}><p>This is profile page</p></div>
+        <Switch>
+          <Route exact path="/profile" component={ProfileList} />
+          <Route exact path="/profile/edit" component={ProfileEdit} />
+          <Route exact path="/profile/:id(\d+)" component={withProfile(ProfileSingle2)} />
+          <Route path="*" component={props => (<p>404</p>)} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        this is page About
+      </div>
+    )
+  }
+}
+
+@observer
 class App extends Component {
+  
+  state = {
+    count: 0,
+    page: 'home',
+    a: 1,
+    b: 2
+  }
+
+  renderPages(page) {
+    switch(page) {
+      case 'home': 
+        return <Home />
+      case 'profile':
+        return <Profile />
+      case 'about':
+        return <About />
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={increaseCount}>Increase Score</button>
+        <Route exact path="/" component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route exact path="/about" component={About} />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer'
+import App, {ProfileSingle2} from './App';
+import ProfileStore from './ProfileStore'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('ProfileSingle2', () => {
+  it('should render placeholder when no profile', done => {
+    const mockProps = {
+      store: ProfileStore
+    }
+    const snapshot = renderer.create(<ProfileSingle2 {...mockProps} />).toJSON()
+    expect(snapshot).toMatchSnapshot()
+    done()
+  })
+
+  it('should render profile if has profile', done => {
+    const mockProps = {
+      store: ProfileStore
+    }
+    ProfileStore.profile = {id: 1, name: 'test'}
+    const snapshot = renderer.create(<ProfileSingle2 {...mockProps} />).toJSON()
+    expect(snapshot).toMatchSnapshot()
+    done()
+  })
+})
